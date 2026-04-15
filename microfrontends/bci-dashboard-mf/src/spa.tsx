f80682f831c8137ade4b5f3e34f678e24a55478a
Carlos Luis <marincarlosl19@gmail.com>
2026-04-15 09:53:14 -0500

    feat: utilizando vite para el mf, validando conexion inicial

---
+++ microfrontends/bci-dashboard-mf/src/spa.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import singleSpaReact from "single-spa-react";
+import App from "./App";
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOMClient: ReactDOM,
+  rootComponent: App,
+  errorBoundary() {
+    return <div>Error loading MF</div>;
+  },
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
